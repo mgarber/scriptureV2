ff13356b4fb6d29828308294736cb4319e29ce4c
sabahkadri <skadri@broadinstitute.org>
2013-03-26 22:09:02 -0400

    Two new read filters: NoSpliceFilter, PairedAndProperFilter(used by Scripture) added. CanonicalSpliceFilter edits minor.

---
+++ src/java/nextgen/core/readFilters/NoSpliceFilter.java
@@ -0,0 +1,25 @@
+package nextgen.core.readFilters;
+
+import nextgen.core.alignment.Alignment;
+
+import org.apache.commons.collections15.Predicate;
+
+public class NoSpliceFilter implements Predicate<Alignment>{
+
+	@Override
+	public boolean evaluate(Alignment align) {
+		//if has does not have splice junctions --> return true
+		if(align.getSpliceConnections().isEmpty()){
+			return true;
+		}
+		return false;
+	}
+
+	public boolean equals(Object other){
+		if(other instanceof SplicedReadFilter){
+			return true;
+		}
+		return false;
+	}
+	
+}
