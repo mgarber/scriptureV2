c5ee9c775aa8ad208204776be5e47e7b9e18160b
engreitz <engreitz@broadinstitute.org>
2013-04-03 14:41:32 -0400

    Test nonconflicting merge

--- src/java/nextgen/core/model/AlignmentModel.java
+++ src/java/nextgen/core/model/AlignmentModel.java
@@ -49,6 +49,7 @@ import nextgen.core.model.score.WindowScoreIterator;
  * This will be a bridge to the analysis modules
  * @author skadri
  */
+// Second comment added in Eclipse
 public class AlignmentModel extends AbstractAnnotationCollection<Alignment> {
 	// This is a test comment by Jesse and EDITED IN ECLIPSE
 	static Logger logger = Logger.getLogger(AlignmentModel.class.getName());
