a595fad66b9b284c6d07d37d5c81792565a35e21
engreitz <jesse.engreitz@gmail.com>
2013-04-03 15:41:03 -0300

    Test merge

--- src/java/nextgen/core/model/AlignmentModel.java
+++ src/java/nextgen/core/model/AlignmentModel.java
@@ -51,6 +51,7 @@ import nextgen.core.model.score.WindowScoreIterator;
  */
 public class AlignmentModel extends AbstractAnnotationCollection<Alignment> {
 	// This is a test comment by Jesse and EDITED IN ECLIPSE
+	// Second comment added online
 	static Logger logger = Logger.getLogger(AlignmentModel.class.getName());
 	protected CoordinateSpace coordinateSpace;
 	PairedEndReader  reader;
