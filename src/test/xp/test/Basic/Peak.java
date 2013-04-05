6928880b932a0fc9ce1562915f7d5e040811ca3b
zhuxp <nimezhu@gmail.com>
2013-04-05 13:57:19 -0400

    add some code to CallPeak

---
+++ src/test/xp/test/Basic/Peak.java
@@ -0,0 +1,176 @@
+
+package xp.test.Basic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+/**
+ *  Created on 2013-4-1  
+ */
+
+public class Peak {
+	private static final Logger logger = Logger.getLogger(Peak.class);
+	private List<BedGraphMultiScore> listBedGraph;
+	private List<Double>  listScores;
+	//private List<FocusAndEnv> listFocusAndEnv;
+	private long  start;
+	private long end;
+	private String chr=null;
+	private int tid;
+	private String name="chr";
+	private double score=0.0; //default is max of scores
+	
+	public Peak()
+	{
+		listBedGraph=new ArrayList<BedGraphMultiScore>();
+		listScores= new ArrayList<Double>();
+		
+	}
+	public Peak(String name)
+	{
+		this();
+		this.setName(name);
+		
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getScore() {
+		return score;
+	}
+	public void setScore(double score) {
+		this.score = score;
+	}
+	public boolean isEmpty()
+	{
+		if (listBedGraph.size()==0)
+				return true;
+		return false;
+	}
+	
+	public int add(BedGraphMultiScore b)
+	{
+		if (listBedGraph.size()==0)
+		{
+			listBedGraph.add(b);
+			start=(long)b.getStart();
+			end=(long)b.getEnd();
+			tid=b.getTid();
+		}
+		else
+		{
+			//assert(tid==b.getTid());
+			if(tid!=b.getTid())
+			{
+				logger.warn("Can't add " + b + " to peak" + this +"\n"+ "Skipping adding" + b);
+				return 0;
+			}
+			if(start > b.getStart()) start=(long)b.getStart();
+			if(end < b.getEnd()) end=(long)b.getEnd();
+			
+			listBedGraph.add(b);
+			
+		}
+		return 1;
+	}
+	
+	
+	public void add(BedGraphMultiScore b, double score)
+	{
+		int a=add(b);
+		if(a==1) listScores.add(score);
+		if(this.score<score) 
+			this.score=score;
+		
+	}
+	
+	
+	public long getStart() {
+		return start;
+	}
+	public long getEnd() {
+		return end;
+	}
+	public String getChr() {
+		return chr;
+	}
+	public int getTid()
+	{
+		return tid;
+	}
+	public void setTid(int tid)
+	{
+		this.tid=tid;
+	}
+	public void setChr(String c)
+	{
+		chr=c;
+	}
+	
+	
+	public int length()
+	{
+		int len = (int) (getEnd()-getStart());
+		return len;
+	}
+	public int getSampleSize()
+	{
+		return listBedGraph.get(0).getScore().length;
+	}
+	public double[] getAverageScores()
+	{
+		double[] scores = new double[getSampleSize()];
+		for(BedGraphMultiScore i : listBedGraph)
+		{
+			for (int j = 0; j < scores.length; j++) {
+				scores[j]+=(double)i.getScore()[j]*i.length();
+			}
+		}
+		for (int i = 0; i < scores.length; i++) {
+			scores[i]/=length();
+		}
+		return scores;
+		
+	}
+	public int[] getSumScores()
+	{
+		int[] scores = new int[getSampleSize()];
+		for(BedGraphMultiScore i : listBedGraph)
+		{
+			for (int j = 0; j < scores.length; j++) {
+				scores[j]+=(double)i.getScore()[j]*i.length();
+			}
+		}
+		return scores;	
+	}
+	/**
+	 * 
+	 * @param lambdas
+	 * @return normalized coverage.
+	 */
+	public double[] getNormalizedScores(double[] lambdas)
+	{
+		double[] scores=getAverageScores();
+		assert(scores.length==lambdas.length);
+		for (int i = 0; i < scores.length; i++) {
+			scores[i]/=lambdas[i];
+		}
+		return scores;	
+	}
+	
+	public String toSimpleBED()
+	{
+		
+		return String.format("%s\t%d\t%d\t%s\t%.2f", chr,start,end,name,score);
+	}
+	
+	
+	
+	
+ }
---
+++ src/test/xp/test/Basic/SkellamScoreMachine.java
@@ -0,0 +1,176 @@
+
+package xp.test.Basic;
+
+import org.apache.log4j.Logger;
+
+import broad.core.math.Distribution;
+
+/**
+ *  Created on 2013-4-2  
+ */
+
+public class SkellamScoreMachine implements ScoreMachine {
+	private double threshold = 8.0;
+	private double MAXSCORE=300.0;
+	private static final Logger logger = Logger
+			.getLogger(SkellamScoreMachine.class);
+
+	@Override
+	public double getScore(double[] x) {
+		// TODO Auto-generated method stub
+		// NOT IMPLEMENT
+		return 0;
+	}
+
+	@Override
+	public double getScore(double[] x, double[] background) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getScore(double[] x, double[] globalBackground,
+			double[] localBackGround) {
+		// TODO Auto-generated method stub
+	    int sampleNumber=x.length;
+	    if(sampleNumber==2)
+	    {
+	    	return getScore2(x,globalBackground,localBackGround);
+	    	
+	    }
+	    else if (sampleNumber==4)
+	    {
+	    	return getScore4(x,globalBackground,localBackGround);
+	    }
+	    else
+	    {
+	    	logger.warn("Wrong Sample Number, not implement yet");
+	    }
+		return 0;
+	}
+
+	@Override
+	public double getThreshold() {
+		 return threshold;
+	}
+
+	@Override
+	public void setThreshold(double t) {
+	     this.threshold=t;	
+	}
+	
+	
+	private double getScore2(double[] x, double[] globalBackground,double[] localBackground)
+	{
+		/*
+		 * ORDER:
+		 * Case , Control
+		 *   x[0] is Case SignalCount
+		 *   x[1] is Control backgroundCount
+		 *   
+		 *   globalBackground[0] is Case global background lambda
+		 *   globalBackground[1] is Control global background lambda
+		 *   
+		 *   localBackground[0] is Case local background lambda
+		 *   localBackground[1] is Control local background lambda
+		 *   
+		 */
+		Double globalScore;
+		Double localScore;
+		globalScore=getSkellamScore((int)Math.round(x[0]),globalBackground[0],(int)Math.round(x[1]),globalBackground[1]);
+		localScore=getSkellamScore((int)Math.round(x[0]),localBackground[0],(int)Math.round(x[1]),localBackground[1]);
+		double score;
+		if(localScore==null)
+			score=globalScore;
+		else
+			score=Math.min(globalScore, localScore);
+		return score;
+	}
+	private double getScore4(double[] x,double[] globalBackground,double[] localBackground)
+	{
+		/*
+		 *  order:
+		 *  CaseA, ControlA, CaseB, ControlB
+		 *  
+		 *  x[0] caseA count
+		 *  x[1] controlA count
+		 *  x[2] caseB count
+		 *  x[3] conttrolB count
+		 *  
+		 *  globalBackground[0]  caseA global lambda
+		 *  globalBackground[1]  controlA global lambda
+		 *  globalBackground[2]  caseB global lambda
+		 *  globalBackground[3]  controlB global lambda
+		 *  
+		 *  localBackground[0]   caseA local lambda
+		 *  localBackground[1]   controlA local lambda
+		 *  localBackground[2]   caseB local lambda
+		 *  localBackground[3]   controlB local lambda
+		 *  
+		 *  
+		 *  Define Score is Max Score of  
+		 *          caseA vs controlA
+		 *          caseB vs controlB
+		 *          caseA + caseB  vs  controlA + controlB
+		 */
+        
+		double[] tX= new double[2];
+		double[] tGlobalBackground = new double[2];
+		double[] tLocalBackground = new double[2];
+		
+		double scoreA;
+		double scoreB;
+		double scoreAB;
+		
+		// CaseA vs Control A
+		tX[0]=x[0];
+		tX[1]=x[1];
+		tGlobalBackground[0]=globalBackground[0];
+		tGlobalBackground[1]=globalBackground[1];
+        tLocalBackground[0]=localBackground[0];
+        tLocalBackground[1]=localBackground[1];
+		scoreA=getScore2(tX,tGlobalBackground,tLocalBackground);
+		// CaseB vs Control B
+		tX[0]=x[2];
+		tX[1]=x[3];
+		tGlobalBackground[0]=globalBackground[2];
+		tGlobalBackground[1]=globalBackground[3];
+        tLocalBackground[0]=localBackground[2];
+        tLocalBackground[1]=localBackground[3];
+		scoreB=getScore2(tX,tGlobalBackground,tLocalBackground);
+			
+		// CaseA + CaseB vs Control A + Control B
+		tX[0]+=x[0];
+		tX[1]+=x[1];
+		tGlobalBackground[0]+=globalBackground[0];
+		tGlobalBackground[1]+=globalBackground[1];
+        tLocalBackground[0]+=localBackground[0];
+        tLocalBackground[1]+=localBackground[1];
+		scoreAB=getScore2(tX,tGlobalBackground,tLocalBackground);
+		
+		double score;
+		score=Math.max(scoreA, scoreB);
+		score=Math.max(score, scoreAB);
+		return score;
+		
+	}
+	
+	
+	private Double getSkellamScore(int signalCount, double signalLambda, int controlCount, double controlLambda)  {
+		int k=signalCount-controlCount;
+		if(controlLambda==0 || signalLambda==0)
+		{
+			return null;
+		}
+		
+		double rightPvalue = Distribution.skellamRightTail(k, signalLambda, controlLambda);
+		double score=-Math.log10(rightPvalue);
+	    	if( score < MAXSCORE)
+	    		return score;
+	    	else
+	    		return MAXSCORE;
+	    
+	  }
+	
+
+}
