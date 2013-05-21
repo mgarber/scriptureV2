f276ff3e9874baaca8521fd35dbd7ca0c3eed05f
Pamela Russell <pamela.russell@gmail.com>
2013-05-21 17:06:54 -0400

    Another implementation of normalization interface

---
+++ src/java/nextgen/core/utils/FileUtil.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package nextgen.core.utils;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * @author prussell
+ *
+ */
+public final class FileUtil {
+
+	/**
+	 * Get the lines of a file as a list
+	 * @param fileName File name
+	 * @return The lines of the file in a list of strings
+	 * @throws IOException
+	 */
+	public static ArrayList<String> fileLinesAsList(String fileName) throws IOException {
+		ArrayList<String> rtrn = new ArrayList<String>();
+		FileReader r = new FileReader(fileName);
+		BufferedReader b = new BufferedReader(r);
+		while(b.ready()) {
+			String line = b.readLine();
+			if(line.length() > 0) {
+				rtrn.add(line);
+			}
+		}
+		r.close();
+		b.close();
+		return rtrn;
+	}
+
+}
