36eb07d777b79f5e06bda055470561b91dc09d8a
Manuel Garber <manuel.garber@umassmed.edu>
2013-08-23 11:14:29 -0400

    Preliminary commit of performance changes

---
+++ src/test/nextgen/core/scripture/MakePairsTest.java
@@ -0,0 +1,12 @@
+package nextgen.core.scripture;
+
+import net.sf.samtools.SAMRecord;
+import junit.framework.TestCase;
+
+public class MakePairsTest extends TestCase {
+	
+	public void testPaireMaking() {
+		SAMRecord p1 = new SAMRecord();
+	}
+
+}
